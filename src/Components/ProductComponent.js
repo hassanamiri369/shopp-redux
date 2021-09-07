d06cb52b6496e67ee7147918da9b9df8a92cd5f7
hassanamiri369 <hassanamiri369@gmail.com>
2021-09-07 17:49:30 +0330

    create new component for show all product

---
+++ src/Components/ProductComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./ProductComponent.css";
+
+
+const ProductComponent = () => {
+    return (
+        <div>
+            Product Components
+        </div>
+    )
+}
+
+export default ProductComponent
