68ceacfcd704380fd62879efa55b6f1dbd75179c
hassanamiri369 <hassanamiri369@gmail.com>
2021-09-07 14:04:09 +0330

    add middleware thunk

---
+++ src/Redux/Store.js
@@ -0,0 +1,13 @@
+import { createStore , applyMiddleware } from "redux";
+
+// thunk middleware
+
+import thunk from "redux-thunk";
+// import combine reducers 
+
+import reducers from "./AllReducers.js/index";
+
+
+const store = createStore(reducers , {} , applyMiddleware(thunk))
+
+export default store
