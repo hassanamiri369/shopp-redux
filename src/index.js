3bd33f577b487fdf75dec2a8235feaecf38375b4
hassanamiri369 <hassanamiri369@gmail.com>
2021-09-07 17:56:43 +0330

    added bootstrap style file

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import store from "./Redux/Store";
 
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css";
 import App from "./App";
 
