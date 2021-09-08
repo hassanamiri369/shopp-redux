863d3d96857b6133208f4f592a67477d48324c69
hassanamiri369 <hassanamiri369@gmail.com>
2021-09-08 15:03:21 +0330

    added Header

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from "react-router-dom"
+
+const Header = () => {
+    return (
+        <>
+            <h2><Link to="/">Shopping</Link></h2>
+        </>
+    )
+}
+
+export default Header
